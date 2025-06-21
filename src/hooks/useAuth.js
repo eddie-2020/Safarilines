523195eab1039c370b130a0e8da9a4c8b083a2de
Eddie <oluyaratosin123@gmail.com>
2025-06-21 03:22:30 +0100

    first commit

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -88,7 +88,7 @@ const useAuth = (switchToLogin) => {
         document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
       });
       
-      navigate('/login');
+      navigate('/');
       toast.success('Logged out successfully');
       
     } catch (error) {
